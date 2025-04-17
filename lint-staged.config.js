1a9738240eea95a41735ee78a45ca412c459e71a
BlessSan <blessius.septian@gmail.com>
2025-04-17 18:35:48 +0800

    chore: update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 // Root lint-staged.config.js
 module.exports = {
   "frontend/**/*.{ts,tsx}": [
-    "cd frontend && eslint --fix",
-    "cd frontend && prettier --write",
-    () => "cd frontend && tsc-files --noEmit",
+    "npx eslint --fix",
+    "npx prettier --write",
+    () => "npx tsc-files --noEmit",
   ],
   "backend/**/*.ts": [
-    "cd backend && eslint --fix",
-    "cd backend && prettier --write",
-    () => "cd backend && tsc-files --noEmit",
+    "npx eslint --fix",
+    "npx prettier --write",
+    () => "npx tsc-files --noEmit",
   ],
-  "{frontend,backend}/**/*.{json,css,md}": ["prettier --write"],
+  "{frontend,backend}/**/*.{json,css,md}": ["npx prettier --write"],
 };
